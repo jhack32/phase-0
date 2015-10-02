c6c8784b6e2d7a186c41101b4263e133a63242d9
Jack Huang <Jack7100@gmail.com>
2015-10-02 13:03:58 -0400

    Add more solution to problem

--- week-4/shortest-string/my_solution.rb
+++ week-4/shortest-string/my_solution.rb
@@ -15,6 +15,8 @@ def shortest_string(list_of_words)
   # Your code goes here!
   return list_of_words[0].to_s if list_of_words.length == 1
   smallest_word(list_of_words)
+#  until_loop(list_of_words)
+#  for_loop(list_of_words)
 #  smallest = list_of_words[0]
 #  list_of_words.each do |word|
 #    smallest = word if word.length < smallest.length
@@ -22,10 +24,30 @@ def shortest_string(list_of_words)
 # return smallest
 end
 
+#solved with each loop
 def smallest_word(array)
   smallest = array[0]
   array.each do |word|
     smallest = word if word.length < smallest.length
   end
   return smallest
+end
+
+#solved with until loop
+def until_loop(array)
+  shortest = array[0]
+  until array.empty?
+   word = array.shift
+   shortest = word if shortest.length > word.length
+  end
+  return shortest
+end
+
+#solved with for loop
+def for_loop(array)
+  shortest = array[0]
+  for array in array[0..array.length]
+    shortest = array if shortest.length > array.length
+  end
+  return shortest
 end
