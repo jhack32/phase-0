c0476af4fad97ddfbf00b1bee5f0a69569663b0a
Jack Huang <Jack7100@gmail.com>
2015-10-05 22:24:58 -0400

    Add pseudocode

---
+++ week-5/gps2_2.rb
@@ -0,0 +1,21 @@
+#Pseudocode for GPS 2.2
+
+#define a method for new list with a hash argument (1 argument)
+#create local variable (list) equal to hash
+#END the method
+
+#define a method to add key and quantity to the hash (3 arguments)
+#add key (itemname) giving it a quantity of 3
+#END the method
+
+#define a method to remove a item from hash (2 argument)
+#remove itemname
+#END the method
+
+#define a method to update quantities for our hash (3 arguments)
+#access the key with itemname changing the quantity new quantity
+#END method
+
+#define a method to print the hash (1 argument)
+#print the local variable (list)
+#END method
