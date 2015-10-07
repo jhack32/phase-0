6e20d13645fbc81c802de9e942e75e5e7cdfbe4d
Jack Huang <Jack7100@gmail.com>
2015-10-07 14:00:31 -0400

    Add pair's name

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,9 +1,9 @@
 =begin
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge [with: Eric Dell'Aringa]
 
-# I spent [] hours on this challenge.
+# I spent [1.2] hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
