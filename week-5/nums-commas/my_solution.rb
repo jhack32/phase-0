618a6840f2bbf4c29be041e28750459139773031
Jack Huang <Jack7100@gmail.com>
2015-10-08 20:11:41 -0400

    Complete a better refactor solution

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -65,9 +65,9 @@ end
 =end
 
 # 2. Refactored Solution
-
-
-
+def separate_comma(number)
+  number.to_s.reverse.split("").each_with_index { |x,y| x << "," if y == 3 || y == 6 }.reverse.join
+end
 
 =begin
 #used multiple built-in methods (to_s, reverse, gsub)
@@ -83,23 +83,24 @@ After figuring out the regex I had to use to find the correct placement, I just 
 
 # 3. Reflection
 
-What was your process for breaking the problem down? What different approaches did you consider?
+>What was your process for breaking the problem down? What different approaches did you consider?
 
 I needed to rewrite my pseudocode. The first time around, my pseudocode wasn't that good. I realized that i needed to break down the problem into smaller parts. My initial pseudocode was very vague when I looked back on it. Plus, it was incorrect for this challenge because I'm supposed to use an iteration.
 
-Was your pseudocode effective in helping you build a successful initial solution?
+>Was your pseudocode effective in helping you build a successful initial solution?
 
 Yes, it definitely helped me. I looked back at it frequently to figure out what I should do. It gave me a good start to the solution and helped me along the way.
 
-What Ruby method(s) did you use when refactoring your solution? What difficulties did you have implementing it/them? Did it/they significantly change the way your code works? If so, how?
+>What Ruby method(s) did you use when refactoring your solution? What difficulties did you have implementing it/them? Did it/they significantly change the way your code works? If so, how?
 
-
+I had to convert the number into a string in order to separate it as an array. So i used .to_s/.reverse/.split so that when it's looping, it'll be from the end of the number to the front. Then, I used a each_with_index iteration to loop through the array, and add commas at a certain index. Finally, i reversed it back and joined the numbers together.
 
 How did you initially iterate through the data structure?
 
-
+I initially used an each iteration with conditional statements (if/unless).
 
 Do you feel your refactored solution is more readable than your initial solution? Why?
 
+I think the refactored solution is readable. It goes in order of what it's going. So on the number, it will convert it into a string, reverse it, split it into separate numbers, and iterate through it with indexes (adding commas in the 3rd and 6th index), then reverse it back to the original and finally joining it together. That's what I read when i looked through it.
 
-
+=end
