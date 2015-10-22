2af354d8bd4c050a25a48577c86f98ddb820d8bc
Jack Huang <Jack7100@gmail.com>
2015-10-21 20:54:23 -0400

    Complete reflection

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -62,18 +62,18 @@ console.log(separateComma(123456789123))
 /*
 What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
 
-Our approach to the problem was the same but we did have issues writing the problem. We wrote our
+Our approach to the problem was the same as if we were to approach it if we were writing it in Ruby. We started with pseudocode and thought of it like any other problem. We broke it down. We did have issues with our loop. It was the placement of our commas and where we want it to be placed when it's looping.
 
 What did you learn about iterating over arrays in JavaScript?
 
-
+The scope of variables is important. When we defined a variable in our loop, it should be defined before our loop and called inside the loop. If we defined it in the loop, it would only be able to be used inside the loop.
 
 What was different about solving this problem in JavaScript?
 
-
+The syntax definitely threw us off a little. The methods and everything was the same, but there were some rules in JavaScript that doesn't apply in Ruby. The semicolons and parenthesis can make a difference in the code.
 
 What built-in methods did you find to incorporate in your refactored solution?
 
-
+We used the for loop in the refactored solution. It's very similar to the while loop with a counter. We also found splice which was a new method we learned in this challenge.
 
 */
