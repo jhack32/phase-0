e517318e2da7b9621ad73788fa0cc19954bb85c3
Jack Huang <Jack7100@gmail.com>
2015-10-21 20:39:37 -0400

    Complete refactor

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -28,10 +28,6 @@ function separateComma(i) {
       myArray.splice(counter, 0, ",");
       counter += 4
     }
- //   for (var index = 0; index < myArray.length; index += 2) {
-  //    myArray.splice(0, 3, ",")
-  //  }
-  }
   return myArray.reverse().join("")
 }
 
@@ -40,17 +36,16 @@ function separateComma(i) {
 
 function separateComma(i) {
   var myArray = [];
-  var counter = 3;
   if (i < 1000) {
     return i;
   }
   else {
     myArray = i.toString().split('').reverse()
-    while (counter < myArray.length) {
-      myArray.splice(counter, 0, ",")
-      counter += 4
+    for (var index = 3; index < myArray.length; index += 4) {
+      myArray.splice(index, 0, ",");
+      }
+    return myArray.reverse().join("");
     }
-    return myArray.reverse().join("")
   }
 }
 
@@ -64,3 +59,21 @@ console.log(separateComma(12345678923))
 console.log(separateComma(123456789123))
 
 // Reflection
+/*
+What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+
+Our approach to the problem was the same but we did have issues writing the problem. We wrote our
+
+What did you learn about iterating over arrays in JavaScript?
+
+
+
+What was different about solving this problem in JavaScript?
+
+
+
+What built-in methods did you find to incorporate in your refactored solution?
+
+
+
+*/
