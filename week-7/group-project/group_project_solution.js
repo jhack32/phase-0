e7cdeacd11b627a180b95274cc6cac42d0d905ac
Jack Huang <Jack7100@gmail.com>
2015-10-24 22:29:40 -0400

    Complete refacor/explanation

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -23,23 +23,51 @@ create function "calculate" that take a list of sorted scores
     set median equal to list with the num index
   end the statement
 end the function
+INITIAL CODE*/
+///////////////////////////////////////////////
+function calculate(scores) {
+  var sum = 0;
+  var median = 0;
 
-INITIAL CODE
+  for (var i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  };
+
+  var mean = sum / scores.length;
+
+  if(scores.length % 2 === 0) {
+    var num = scores.length / 2;
+    median = (scores[num] + scores[num + 1]) / 2;
+  }
+  else {
+    var num = scores.length / 2;
+    median = scores[num];
+  }
+}
+//REFACTORED CODE
 ///////////////////////////////////////////////
 
+function calculate(scores) {
+  var sum = 0;
+  var median = 0;
+  var num = scores.length / 2;
+  for (var i = 0; i < scores.length; i++) {
+    sum += scores[i];
+  };
+  var mean = sum / scores.length;
+  if (scores.length % 2 === 0) {
+    median = (scores[num] + scores[num + 1]) / 2;
+  }
+  else {
+    median = scores[num];
+  }
+  console.log(median)
+}
+//calculate([1000,20000,30000])
+/*
 
-
-
-
-
-
-REFACTORED CODE
-///////////////////////////////////////////////
-
-
-
-
-
-
+The program takes an argument (scores). Then it creates the sum and median variables which are set to 0. The variable num is equal to the length of scores divided by 2.
+Then a loop was created to add the sum of the numbers in the array and put it into a new variable (sum). The mean variable took the sum and divided it by the length of the scores.
+There was an issue with the median. If we have an even number of scores, it works but if we have an odd number (our else statement) it won't work. We will get undefined because if we have 3 scores, our num will equal to 1.5 and there's no 1.5 index. In our else statement, if we have 3 numbers, median will equal to scores[1.5]. That's not a valid index number.
 
 */
